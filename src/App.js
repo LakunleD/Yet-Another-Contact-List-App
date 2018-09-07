aa9cb973315d12b6c972bd5b07892d279b3d1a0e
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2018-09-07 11:18:03 +0100

    moved the components into a folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import FriendsList from './FriendsList';
-import AddFriend from './AddFriend';
+import FriendsList from './components/FriendsList';
+import AddFriend from './components/AddFriend';
 
 class App extends React.Component {
     constructor(props) {
